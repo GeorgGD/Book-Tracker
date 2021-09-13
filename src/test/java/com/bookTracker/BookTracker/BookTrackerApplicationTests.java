f92e6b4e0c1bb128238b0e18e336f102e3a83512
GeorgGD <giorgos_davak@hotmail.com>
2021-09-13 22:55:33 +0200

    fix: create spring boot application

---
+++ src/test/java/com/bookTracker/BookTracker/BookTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookTracker.BookTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
