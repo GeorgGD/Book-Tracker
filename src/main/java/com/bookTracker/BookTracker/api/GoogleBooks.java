18750b173c7ad6552693973eb6836cf31eb8c85f
GeorgGD <giorgos_davak@hotmail.com>
2021-09-14 16:08:58 +0200

    doc: document the interface and it's methods

--- src/main/java/com/bookTracker/BookTracker/api/GoogleBooks.java
+++ src/main/java/com/bookTracker/BookTracker/api/GoogleBooks.java
@@ -5,12 +5,30 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * A google books api class that searches for books
+ * @auther Georgios Davakos
+ * @since 2021-09-14
+ */
 @Component
 public interface GoogleBooks {
 
+	/**
+	 * Closes the client used for api calls
+	 */	
 	public void closeClient();
 
+	/**
+	 * Searches for books based on the given search query
+	 * @param query The search query
+	 * @return Optional list of books matching the search query	
+	 */		
 	public Optional<List<BookSearch>> searchBook(String query);
 
+	/**
+	 * Collects information about a given book
+	 * @param bookID The is of the book
+	 * @return The book if the given book id exists	
+	 */
 	public Optional<Book> bookInfo(String bookId);
 }
