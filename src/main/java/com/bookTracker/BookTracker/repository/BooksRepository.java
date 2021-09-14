59345d8171390e235433691b65ef9f13700daa5a
GeorgGD <giorgos_davak@hotmail.com>
2021-09-14 20:58:38 +0200

    doc: write the correct name of the table

--- src/main/java/com/bookTracker/BookTracker/repository/BooksRepository.java
+++ src/main/java/com/bookTracker/BookTracker/repository/BooksRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Connects to external database and has CRUD access over the books table
+ * Connects to external database and has CRUD access over the library table
  * @author Georgios Davakos
  * @since 2021-09-14 
  */
