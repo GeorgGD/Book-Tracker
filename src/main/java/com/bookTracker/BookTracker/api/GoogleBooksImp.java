8d359fa74bb61fb24a85ece4dd843ee1884aaaf4
GeorgGD <giorgos_davak@hotmail.com>
2021-09-17 22:09:31 +0200

    cut: remove autowired import

--- src/main/java/com/bookTracker/BookTracker/api/GoogleBooksImp.java
+++ src/main/java/com/bookTracker/BookTracker/api/GoogleBooksImp.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
