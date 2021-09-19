84a2508b674320e620c491d9914a26af6853c12a
GeorgGD <giorgos_davak@hotmail.com>
2021-09-19 20:44:52 +0200

    fix: fix incorrect use of MediaType.get()
    By adding the correct argument for MediaType a bunch more bugs
    showed themselfs in the system and there for had to be fixed
    as well.
    
    The code section "when(client.newCall(bookSearch).execute())"
    would cause NullPointerException cause the return value for
    "client.newCall(bookSearch)" had not been defined. SO, setupCall
    was created to solve this issue.

--- src/test/java/com/bookTracker/BookTracker/api/GoogleBooksImpTest.java
+++ src/test/java/com/bookTracker/BookTracker/api/GoogleBooksImpTest.java
@@ -9,27 +9,38 @@ import java.io.IOException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+@SpringBootTest
 public class GoogleBooksImpTest {
 
 	private GoogleBooksImp googleBooksImp;
 	private OkHttpClient client;
+	private Call call;
 
 	private void setupClient() throws IOException {
-		client = mock(OkHttpClient.class);
+		client = mock(OkHttpClient.class);		
 		String url = "https://www.googleapis.com/books/v1/volumes?q=courage+is+calling&key=no-key";
 		Request bookSearch = setupSearchBookRequest(url);
-		Response response = setupSearchBookResponse("bookSearchJSON.txt");
-		when(client.newCall(bookSearch).execute()).thenReturn(response);
+		when(client.newCall(bookSearch)).thenReturn(call);
 	}
 
+	private void setupCall() throws IOException {
+		call = mock(Call.class);
+		Response response = setupSearchBookResponse("../bookSearchJSON.txt", new Request.Builder().url("http://www.notneededurl.com").get().build());
+		when(call.execute()).thenReturn(response);
+	}
+	
 	private Request setupSearchBookRequest(String url) {
 		Request request = new Request.Builder()
 			.url(url)
@@ -38,9 +49,13 @@ public class GoogleBooksImpTest {
 		return request;
 	}
 	
-	private Response setupSearchBookResponse(String fileName) {
+	private Response setupSearchBookResponse(String fileName, Request request) {
 		Response response = new Response.Builder()
-			.body(ResponseBody.create(MediaType.get("application/json"), readJsonFile(fileName)))
+			.request(request)
+			.protocol(Protocol.HTTP_1_0)
+			.code(200)
+			.message("")
+			.body(ResponseBody.create(MediaType.get("application/json; charset=utf-8"), readJsonFile(fileName)))
 			.build();
 		return response;
 	}
@@ -57,21 +72,18 @@ public class GoogleBooksImpTest {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(0);
 		}
 		return text;
 	}
 	
-	@Before
-	private GoogleBooksImp createGoogleBooksImp() {
+	@BeforeEach
+    public void createGoogleBooksImp() {
 		try {
+			setupCall();
 			setupClient();
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.exit(0);
 		}
-		return new GoogleBooksImp(client, new ObjectMapper());
-	}
-
-	
+		googleBooksImp = new GoogleBooksImp(client, new ObjectMapper());
+	}	
 }
