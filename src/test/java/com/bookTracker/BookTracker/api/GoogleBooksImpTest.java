9c3fde717dbdfea43ee1a1388fe1e5e5dee33653
GeorgGD <giorgos_davak@hotmail.com>
2021-09-19 15:11:48 +0200

    add: implement createGoogleBooksImp

--- src/test/java/com/bookTracker/BookTracker/api/GoogleBooksImpTest.java
+++ src/test/java/com/bookTracker/BookTracker/api/GoogleBooksImpTest.java
@@ -2,20 +2,36 @@ package com.bookTracker.BookTracker.api;
 
 import static org.mockito.Mockito.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.Before;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class GoogleBooksImpTest {
 
 	private GoogleBooksImp googleBooksImp;
 	private OkHttpClient client;
-
+	
 	@Before
 	private GoogleBooksImp createGoogleBooksImp() {
-		client = mock(OkHttpClient.class);
+		try {
+			setupClient();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 		return new GoogleBooksImp(client, new ObjectMapper());
 	}
+
+	
 }
