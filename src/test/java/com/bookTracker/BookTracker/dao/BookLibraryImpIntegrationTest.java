5e1679fe8bd777e4f5aae44d05ed4ccedc3c8359
GeorgGD <giorgos_davak@hotmail.com>
2021-09-24 20:22:21 +0200

    add: add instance variables

--- src/test/java/com/bookTracker/BookTracker/dao/BookLibraryImpIntegrationTest.java
+++ src/test/java/com/bookTracker/BookTracker/dao/BookLibraryImpIntegrationTest.java
@@ -1,10 +1,15 @@
 package com.bookTracker.BookTracker.dao;
 
-import com.bookTracker.BookTracker.repository.BooksRepository;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class BookLibraryImpIntegrationTest {
 
+	@Autowired
+	private BookLibrary bookLibrary;
+	
 }
