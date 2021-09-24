7987ccf45254b4266119965c1058d6b1afc04946
GeorgGD <giorgos_davak@hotmail.com>
2021-09-24 20:39:36 +0200

    add: introduce schema

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS library;
+
+CREATE TABLE IF NOT EXISTS library (
+       id INT NOT NULL,
+       name VARCHAR(200) NOT NULL,
+       auther VARCHAR(200),
+       genre VARCHAR(300),
+       reading TINYINT(1),
+       completed_date DATE,
+       cover_img TEXT,
+       PRIMARY KEY (id)
+);
+
