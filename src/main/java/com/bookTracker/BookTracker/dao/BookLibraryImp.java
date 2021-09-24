2441b075a5875af3c826a92e5af646294b87dfcf
GeorgGD <giorgos_davak@hotmail.com>
2021-09-24 14:14:33 +0200

    add: introduce BookLibraryImp

---
+++ src/main/java/com/bookTracker/BookTracker/dao/BookLibraryImp.java
@@ -0,0 +1,42 @@
+package com.bookTracker.BookTracker.dao;
+
+import java.util.List;
+
+import com.bookTracker.BookTracker.model.Book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookLibraryImp implements BookLibrary {
+
+	@Override
+	public List<Book> getAllEntries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createBook(Book entry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBook(Book entry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBook(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int availableID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
