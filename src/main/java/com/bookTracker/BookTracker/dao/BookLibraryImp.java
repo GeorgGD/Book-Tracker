bdf0a06713352ca5f4e6aa0b2055bc084edd207f
GeorgGD <giorgos_davak@hotmail.com>
2021-09-24 16:23:07 +0200

    doc: add documentation

--- src/main/java/com/bookTracker/BookTracker/dao/BookLibraryImp.java
+++ src/main/java/com/bookTracker/BookTracker/dao/BookLibraryImp.java
@@ -6,33 +6,58 @@ import com.bookTracker.BookTracker.model.Book;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Handles all communications to the library database
+ * @author Georgios Davakos
+ * @since 2021-09-24
+ */
 @Service
 public class BookLibraryImp implements BookLibrary {
 
+	/**
+	 * Retrieves all the entries from the database
+	 * @return All the books	
+	 */
 	@Override
 	public List<Book> getAllEntries() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Creates a book in the database
+	 * @param entry The book
+	 */
 	@Override
 	public void createBook(Book entry) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+	 * Updates a book in the database
+	 * @param entry The updated book
+	 */	
 	@Override
 	public void updateBook(Book entry) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Deletes a book from the database
+	 * @param id The id of the book	
+	 */
 	@Override
 	public void deleteBook(Integer id) {
 		// TODO Auto-generated method stub
 		
 	}
 
+   	/**
+	 * Provides an available ID for the book
+	 * @return The available id	
+	 */
 	@Override
 	public int availableID() {
 		// TODO Auto-generated method stub
