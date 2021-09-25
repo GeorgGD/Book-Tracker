c1c5af6afb6490186c9794cdaebe10f045668a22
GeorgGD <giorgos_davak@hotmail.com>
2021-09-25 20:06:23 +0200

    feat: create controller that manages endpoints for book tracker

---
+++ src/main/java/com/bookTracker/BookTracker/controllers/BookLibraryController.java
@@ -0,0 +1,17 @@
+package com.bookTracker.BookTracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * The following controller manages a book tracking system
+ * were a user can save books they have read, are reading or want 
+ * to read in the future
+ * @author Georgios Davakos
+ * @since 2021-09-25
+ */
+@Controller
+@RequestMapping("/")
+public class BookLibraryController {
+	
+}
