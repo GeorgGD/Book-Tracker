eb79454d8cf54c53e022ded4d4248608478ecdad
GeorgGD <giorgos_davak@hotmail.com>
2021-09-25 20:20:08 +0200

    add: create method that saves books from GoogleBooksAPI into the database

--- src/main/java/com/bookTracker/BookTracker/controllers/BookLibraryController.java
+++ src/main/java/com/bookTracker/BookTracker/controllers/BookLibraryController.java
@@ -1,7 +1,11 @@
 package com.bookTracker.BookTracker.controllers;
 
+import java.sql.Date;
+import java.util.Optional;
+
 import com.bookTracker.BookTracker.api.GoogleBooks;
 import com.bookTracker.BookTracker.dao.BookLibrary;
+import com.bookTracker.BookTracker.model.Book;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +26,23 @@ public class BookLibraryController {
 
 	private BookLibrary bookLibrary;
 	private GoogleBooks googleBooks;
+
+	/**
+	 * Saves a book from Google Books API into the database 
+	 * @param id The Google Books API id for the book
+	 * @param reading True if you are reading the book
+	 * @param date The Date you finished reading the book
+	 */
+	private void saveBookIntoDatabase(String id, boolean reading, Date date) {
+		Optional<Book> optional = googleBooks.bookInfo(id);
+
+		if (optional.isPresent()) {
+			Book book = optional.get();
+			book.setReading(reading);
+			book.setCompleted_date(date);
+			book.setId(bookLibrary.availableID());
+
+			bookLibrary.createBook(book);
+		}
+	}
 }
