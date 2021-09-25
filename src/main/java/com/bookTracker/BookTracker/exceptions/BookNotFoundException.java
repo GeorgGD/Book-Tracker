55c2e14971d39f7d607e2d6981e8e8117b3d35ac
GeorgGD <giorgos_davak@hotmail.com>
2021-09-25 23:55:28 +0200

    doc: correct the documentation

--- src/main/java/com/bookTracker/BookTracker/exceptions/BookNotFoundException.java
+++ src/main/java/com/bookTracker/BookTracker/exceptions/BookNotFoundException.java
@@ -1,8 +1,7 @@
 package com.bookTracker.BookTracker.exceptions;
 
 /**
- * Throw this exception when a book is not found from either
- * Google Books API or book tracker database
+ * Throw this exception when a book is not found from book tracker database
  */
 public class BookNotFoundException extends RuntimeException {
 
