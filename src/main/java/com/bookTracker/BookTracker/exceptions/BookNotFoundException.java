d79b011919e059bbbd06bde826198e21a9e77425
GeorgGD <giorgos_davak@hotmail.com>
2021-09-26 19:49:09 +0200

    add: overload the constructor

--- src/main/java/com/bookTracker/BookTracker/exceptions/BookNotFoundException.java
+++ src/main/java/com/bookTracker/BookTracker/exceptions/BookNotFoundException.java
@@ -3,9 +3,14 @@ package com.bookTracker.BookTracker.exceptions;
 /**
  * Throw this exception when a book is not found from book tracker database
  */
+
 public class BookNotFoundException extends RuntimeException {
 
 	public BookNotFoundException(String query) {
 		super("No book was found after using the following query: " + query);
 	}
+
+	public BookNotFoundException(int id) {
+		super("No book was found with the following id: " + id);
+	}	
 }
