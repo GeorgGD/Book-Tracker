9540bf0331042fd9058b6269814bd3bdda0ba5c1
GeorgGD <giorgos_davak@hotmail.com>
2021-09-27 21:15:38 +0200

    fix: finalize the setting up process for the controller and the MockMvc

--- src/test/java/com/bookTracker/BookTracker/controllers/BookLibraryControllerIntegrationTest.java
+++ src/test/java/com/bookTracker/BookTracker/controllers/BookLibraryControllerIntegrationTest.java
@@ -7,28 +7,31 @@ import java.util.List;
 import java.util.Optional;
 
 import com.bookTracker.BookTracker.api.GoogleBooks;
-import com.bookTracker.BookTracker.dao.BookLibraryImp;
+import com.bookTracker.BookTracker.dao.BookLibrary;
 import com.bookTracker.BookTracker.dto.BookSearch;
 import com.bookTracker.BookTracker.model.Book;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-@WebMvcTest(controllers = BookLibraryController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BookLibraryControllerIntegrationTest {
 
 	@Autowired
-	private static BookLibraryController bookLibraryController;
+	private static BookLibrary bookLibrary;
 
-	@MockBean
+	private static BookLibraryController bookLibraryController;
 	private static GoogleBooks googleBooks;
+	private static MockMvc mockMvc;
 
 	private static Optional<List<BookSearch>> createList() {
 		List<BookSearch> list = new ArrayList<>();
@@ -61,6 +64,11 @@ public class BookLibraryControllerIntegrationTest {
 		googleBooks = mock(GoogleBooks.class);
 		when(googleBooks.searchBook(any(String.class))).thenReturn(createList());
 		when(googleBooks.bookInfo(any(String.class))).thenReturn(createBook());
+		bookLibraryController = new BookLibraryController(bookLibrary, googleBooks);
 	}
 
+	@BeforeEach
+	private void setupMockMvc() {
+		mockMvc = MockMvcBuilders.standaloneSetup(bookLibraryController).build();
+	}
 }
