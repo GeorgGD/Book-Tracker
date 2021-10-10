296b944c1e4881d07afda184cadb07985bf41a2f
GeorgGD <giorgos_davak@hotmail.com>
2021-10-10 20:25:20 +0200

    add: bean for RestTemplate

--- src/main/java/com/bookTracker/BookTracker/BookTrackerApplication.java
+++ src/main/java/com/bookTracker/BookTracker/BookTrackerApplication.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import okhttp3.OkHttpClient;
 
@@ -19,6 +20,11 @@ public class BookTrackerApplication {
 	@Bean
 	public ObjectMapper mapper() {
 		return new ObjectMapper();
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 	
 	public static void main(String[] args) {
