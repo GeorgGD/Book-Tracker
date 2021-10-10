38d49c4a52cb8d3754612468b6b74810d19027e8
GeorgGD <giorgos_davak@hotmail.com>
2021-10-10 19:29:07 +0200

    add: root mapping to index view

--- src/main/java/com/bookTracker/BookTracker/controllers/ViewController.java
+++ src/main/java/com/bookTracker/BookTracker/controllers/ViewController.java
@@ -1,5 +1,9 @@
 package com.bookTracker.BookTracker.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /** 
  * This class is meant to simulate a frontend application, this implies that the class
  * is used to serve pages to the user while also communicating with the backend
@@ -7,6 +11,12 @@ package com.bookTracker.BookTracker.controllers;
  * @author Georgios Davakos
  * @since 2021-10-10  
  */
+@Controller
 public class ViewController {
-	
+
+	@GetMapping("/")
+	public String root(Model model) {
+		
+		return "index";
+	}
 }
