45941c15c21c82906486dafceb1982e53be4d2e6
GeorgGD <giorgos_davak@hotmail.com>
2021-10-10 20:31:13 +0200

    cut: import

--- src/main/java/com/bookTracker/BookTracker/BookTrackerApplication.java
+++ src/main/java/com/bookTracker/BookTracker/BookTrackerApplication.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import okhttp3.OkHttpClient;
 
