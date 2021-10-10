b4b3fa5ae598c4d89f547dcdcaf64743589dcda5
GeorgGD <giorgos_davak@hotmail.com>
2021-10-10 19:14:54 +0200

    feat: a class that returns views

---
+++ src/main/java/com/bookTracker/BookTracker/controllers/ViewController.java
@@ -0,0 +1,12 @@
+package com.bookTracker.BookTracker.controllers;
+
+/** 
+ * This class is meant to simulate a frontend application, this implies that the class
+ * is used to serve pages to the user while also communicating with the backend
+ * endpoints.
+ * @author Georgios Davakos
+ * @since 2021-10-10  
+ */
+public class ViewController {
+	
+}
